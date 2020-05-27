15ef37788be1f3fd80d22db7a368e8eecfcec283
Sudhan Playz <55418697+SudhanPlayz@users.noreply.github.com>
2020-05-27 20:09:01 +0530

    Fixed Coppupted html files

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 
 const info = {
-    port: 3128
+    port: 3128 // My pc port fors testing lol
 }
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/views/404.html");
 });
 
 app.listen(info.port).then(console.log("Successfully started website!"))
